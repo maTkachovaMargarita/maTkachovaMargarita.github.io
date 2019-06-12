ddd0e36360771485c632f9b2430b273d44f093c6
Tkachova Margarita <pitonchik.by@gmail.com>
2019-06-12 15:24:54 +0300

    map

--- digitalproducts/app/js/script.js
+++ digitalproducts/app/js/script.js
@@ -1,15 +1,102 @@
 ;(function($){
 	"use strict";
 
+/////////WORKS SLIDER//////////
+$(function () {
+	const owlWorks = $(".works__slider");
+	owlWorks.owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay:true,
+		nav: true,
+		dots: true,
+		navText: [
+		'<span class="arrow-owl arrow-left"></span>',
+		'<span class="arrow-owl arrow-right"></span>'
+		],
+		responsive: {
+			0: {
+				nav: false
+			},
+			768: {
+				nav: false
+			},
+			992: {
+				nav: true
+			}
+		}
+	});
+});
+
+/////////OUR TEAM SLIDER////////
+$(function () {
+	const owlContact = $(".our-team__slider");
+	owlContact.owlCarousel({
+		items: 3,
+		loop: true,
+		autoplay:true,
+		nav: true,
+		dots: false,
+		navText: [
+		'<span class="arrow-owl arrow-left"></span>',
+		'<span class="arrow-owl arrow-right"></span>'
+		],
+		responsive: {
+			0: {
+				items: 1,
+				nav: false
+			},
+			768: {
+				items: 2,
+				nav: false
+			},
+			992: {
+				nav: true
+			}
+		}
+	});
+});
+
+//////FIXED MENU AND BUTTON TO TOP/////////
+$(function() {
+	const topline = $('.top-menu');
+	$(window).scroll(function() {
+		if($(this).scrollTop() != 0) {
+			$('.to-top').fadeIn();
+		} else {
+			$('.to-top').fadeOut();
+		}
+
+		if($(this).scrollTop()) {
+			topline.addClass('fixed');
+		} else {
+			topline.removeClass('fixed');
+		}
+	});
+
+	$('.to-top').click(function() {
+		$('body,html').animate({scrollTop:0},800);
+	});
+
+});
+
+
+//////////BUTTON MENU/////////
+$(function(){
+	$('.btn-menu').click(function(){
+		$('.popUp').toggleClass('js-hidden');
+	});
+});
+
 //////MAP/////////
 $(function initMap() {
 
-	var map;
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -7.9327641, lng: 112.6377829},
-		zoom: 12,
-		disableDefaultUI: true,
-		 styles:[
+    var map;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -7.9327641, lng: 112.6377829},
+        zoom: 12,
+        disableDefaultUI: true,
+         styles:[
     {
         "featureType": "all",
         "elementType": "geometry",
@@ -203,100 +290,14 @@ $(function initMap() {
         ]
     }
 ]
-	});
+    });
 
-	var marker = new google.maps.Marker({
-		position: {lat: -7.9327641, lng: 112.6377829},
-		 map: map,
-		  title: 'Jl. Ikan Piranha Atas'
+    var marker = new google.maps.Marker({
+        position: {lat: -7.9327641, lng: 112.6377829},
+         map: map,
+          title: 'Jl. Ikan Piranha Atas'
 });
 });
 
-/////////WORKS SLIDER//////////
-$(function () {
-	const owlWorks = $(".works__slider");
-	owlWorks.owlCarousel({
-		items: 1,
-		loop: true,
-		autoplay:true,
-		nav: true,
-		dots: true,
-		navText: [
-		'<span class="arrow-owl arrow-left"></span>',
-		'<span class="arrow-owl arrow-right"></span>'
-		],
-		responsive: {
-			0: {
-				nav: false
-			},
-			768: {
-				nav: false
-			},
-			992: {
-				nav: true
-			}
-		}
-	});
-});
-
-/////////OUR TEAM SLIDER////////
-$(function () {
-	const owlContact = $(".our-team__slider");
-	owlContact.owlCarousel({
-		items: 3,
-		loop: true,
-		autoplay:true,
-		nav: true,
-		dots: false,
-		navText: [
-		'<span class="arrow-owl arrow-left"></span>',
-		'<span class="arrow-owl arrow-right"></span>'
-		],
-		responsive: {
-			0: {
-				items: 1,
-				nav: false
-			},
-			768: {
-				items: 2,
-				nav: false
-			},
-			992: {
-				nav: true
-			}
-		}
-	});
-});
-
-//////FIXED MENU AND BUTTON TO TOP/////////
-$(function() {
-	const topline = $('.top-menu');
-	$(window).scroll(function() {
-		if($(this).scrollTop() != 0) {
-			$('.to-top').fadeIn();
-		} else {
-			$('.to-top').fadeOut();
-		}
-
-		if($(this).scrollTop()) {
-			topline.addClass('fixed');
-		} else {
-			topline.removeClass('fixed');
-		}
-	});
-
-	$('.to-top').click(function() {
-		$('body,html').animate({scrollTop:0},800);
-	});
-
-});
-
-
-//////////BUTTON MENU/////////
-$(function(){
-	$('.btn-menu').click(function(){
-		$('.popUp').toggleClass('js-hidden');
-	});
-});
 
 })(jQuery);
