4fceef69f430a5f9a222bda76c7f0574e6b6e329
Tkachova Margarita <pitonchik.by@gmail.com>
2019-06-12 15:46:37 +0300

    initMap fixed

--- digitalproducts/app/js/script.js
+++ digitalproducts/app/js/script.js
@@ -89,215 +89,216 @@ $(function(){
 });
 
 //////MAP/////////
-$(function initMap() {
+
+
+})(jQuery);
+
+function initMap() {
 
     var map;
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -7.9327641, lng: 112.6377829},
         zoom: 12,
         disableDefaultUI: true,
-         styles:[
-    {
-        "featureType": "all",
-        "elementType": "geometry",
-        "stylers": [
+        styles:[
+        {
+            "featureType": "all",
+            "elementType": "geometry",
+            "stylers": [
             {
                 "color": "#c6cde2"
             }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "geometry.stroke",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "geometry.stroke",
+            "stylers": [
             {
                 "color": "#8f9fc6"
             }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.text",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text",
+            "stylers": [
             {
                 "color": "#5f73ab"
             },
             {
                 "visibility": "simplified"
             }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.text.fill",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text.fill",
+            "stylers": [
             {
                 "color": "#5f73ab"
             }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.text.stroke",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text.stroke",
+            "stylers": [
             {
                 "color": "#e1e5f0"
             }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.icon",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.icon",
+            "stylers": [
             {
                 "visibility": "off"
             }
-        ]
-    },
-    {
-        "featureType": "administrative.land_parcel",
-        "elementType": "labels.text.fill",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels.text.fill",
+            "stylers": [
             {
                 "color": "#8f9fc6"
             }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
             {
                 "color": "#eeeeee"
             }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "labels.text.fill",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [
             {
                 "color": "#757575"
             }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [
             {
                 "color": "#c6cde2"
             }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "labels.text.fill",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [
             {
                 "color": "#8f9fc6"
             }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
             {
                 "color": "#ffffff"
             }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [
             {
                 "color": "#c6cde2"
             }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "labels.text.fill",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.text.fill",
+            "stylers": [
             {
                 "color": "#5f73ab"
             }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels.text.fill",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.text.fill",
+            "stylers": [
             {
                 "color": "#5f73ab"
             }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "elementType": "labels.text.fill",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [
             {
                 "color": "#8f9fc6"
             }
-        ]
-    },
-    {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [
             {
                 "color": "#e5e5e5"
             }
-        ]
-    },
-    {
-        "featureType": "transit.station",
-        "elementType": "geometry",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "transit.station",
+            "elementType": "geometry",
+            "stylers": [
             {
                 "color": "#eeeeee"
             }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [
             {
                 "color": "#c9c9c9"
             }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry.fill",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry.fill",
+            "stylers": [
             {
                 "color": "#e1e5f0"
             }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [
             {
                 "color": "#8f9fc6"
             }
+            ]
+        }
         ]
-    }
-]
     });
 
-    var marker = new google.maps.Marker({
-        position: {lat: -7.9327641, lng: 112.6377829},
-         map: map,
-          title: 'Jl. Ikan Piranha Atas'
+var marker = new google.maps.Marker({
+    position: {lat: -7.9327641, lng: 112.6377829},
+    map: map,
+    title: 'Jl. Ikan Piranha Atas'
 });
-});
-
-
-})(jQuery);
+};
