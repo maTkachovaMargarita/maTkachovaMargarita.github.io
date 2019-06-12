56379a8de136226b359a63bc5307c3d1fd88724d
Tkachova Margarita <pitonchik.by@gmail.com>
2019-06-12 15:22:17 +0300

    map

--- digitalproducts/src/js/script.js
+++ digitalproducts/src/js/script.js
@@ -1,6 +1,93 @@
 ;(function($){
 	"use strict";
 
+/////////WORKS SLIDER//////////
+$(function () {
+	const owlWorks = $(".works__slider");
+	owlWorks.owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay:true,
+		nav: true,
+		dots: true,
+		navText: [
+		'<span class="arrow-owl arrow-left"></span>',
+		'<span class="arrow-owl arrow-right"></span>'
+		],
+		responsive: {
+			0: {
+				nav: false
+			},
+			768: {
+				nav: false
+			},
+			992: {
+				nav: true
+			}
+		}
+	});
+});
+
+/////////OUR TEAM SLIDER////////
+$(function () {
+	const owlContact = $(".our-team__slider");
+	owlContact.owlCarousel({
+		items: 3,
+		loop: true,
+		autoplay:true,
+		nav: true,
+		dots: false,
+		navText: [
+		'<span class="arrow-owl arrow-left"></span>',
+		'<span class="arrow-owl arrow-right"></span>'
+		],
+		responsive: {
+			0: {
+				items: 1,
+				nav: false
+			},
+			768: {
+				items: 2,
+				nav: false
+			},
+			992: {
+				nav: true
+			}
+		}
+	});
+});
+
+//////FIXED MENU AND BUTTON TO TOP/////////
+$(function() {
+	const topline = $('.top-menu');
+	$(window).scroll(function() {
+		if($(this).scrollTop() != 0) {
+			$('.to-top').fadeIn();
+		} else {
+			$('.to-top').fadeOut();
+		}
+
+		if($(this).scrollTop()) {
+			topline.addClass('fixed');
+		} else {
+			topline.removeClass('fixed');
+		}
+	});
+
+	$('.to-top').click(function() {
+		$('body,html').animate({scrollTop:0},800);
+	});
+
+});
+
+
+//////////BUTTON MENU/////////
+$(function(){
+	$('.btn-menu').click(function(){
+		$('.popUp').toggleClass('js-hidden');
+	});
+});
+
 //////MAP/////////
 $(function initMap() {
 
@@ -210,93 +297,6 @@ $(function initMap() {
 		 map: map,
 		  title: 'Jl. Ikan Piranha Atas'
 });
-});
-
-/////////WORKS SLIDER//////////
-$(function () {
-	const owlWorks = $(".works__slider");
-	owlWorks.owlCarousel({
-		items: 1,
-		loop: true,
-		autoplay:true,
-		nav: true,
-		dots: true,
-		navText: [
-		'<span class="arrow-owl arrow-left"></span>',
-		'<span class="arrow-owl arrow-right"></span>'
-		],
-		responsive: {
-			0: {
-				nav: false
-			},
-			768: {
-				nav: false
-			},
-			992: {
-				nav: true
-			}
-		}
-	});
-});
-
-/////////OUR TEAM SLIDER////////
-$(function () {
-	const owlContact = $(".our-team__slider");
-	owlContact.owlCarousel({
-		items: 3,
-		loop: true,
-		autoplay:true,
-		nav: true,
-		dots: false,
-		navText: [
-		'<span class="arrow-owl arrow-left"></span>',
-		'<span class="arrow-owl arrow-right"></span>'
-		],
-		responsive: {
-			0: {
-				items: 1,
-				nav: false
-			},
-			768: {
-				items: 2,
-				nav: false
-			},
-			992: {
-				nav: true
-			}
-		}
-	});
-});
-
-//////FIXED MENU AND BUTTON TO TOP/////////
-$(function() {
-	const topline = $('.top-menu');
-	$(window).scroll(function() {
-		if($(this).scrollTop() != 0) {
-			$('.to-top').fadeIn();
-		} else {
-			$('.to-top').fadeOut();
-		}
-
-		if($(this).scrollTop()) {
-			topline.addClass('fixed');
-		} else {
-			topline.removeClass('fixed');
-		}
-	});
-
-	$('.to-top').click(function() {
-		$('body,html').animate({scrollTop:0},800);
-	});
-
-});
-
-
-//////////BUTTON MENU/////////
-$(function(){
-	$('.btn-menu').click(function(){
-		$('.popUp').toggleClass('js-hidden');
-	});
 });
 
 })(jQuery);
